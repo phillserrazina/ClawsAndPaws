241760019e12fb12b22928bd11b8b798014435fd
phillserrazina <fserrazina99@gmail.com>
2019-07-24 17:00:08 +0100

    Added Basic Scene Placeholder Visuals and Scripts
    - Turn Manager & Actor Scripts

---
+++ ClawsAndPawsProject/Assets/Scripts/Entities/Actor.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Actor : MonoBehaviour {
+
+	// VARIABLES
+
+	private float maxHealthPoints = 100f;
+	private float currentHealthPoints;
+
+	private float maxStaminaPoints = 100f;
+	private float currentStaminaPoints;
+
+	// EXECUTION METHODS
+
+	private void Start() {
+		Initialize();
+	}
+
+	// METHODS
+
+	protected void Initialize() {
+		currentHealthPoints = maxHealthPoints;
+		currentStaminaPoints = maxStaminaPoints;
+	}
+
+	public void TakeDamage(float damage) {
+		currentHealthPoints -= damage;
+	}
+}
