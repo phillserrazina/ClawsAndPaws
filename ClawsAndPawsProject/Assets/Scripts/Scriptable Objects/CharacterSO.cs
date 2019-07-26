6d477beadece381ac4f72b4a6f67b70ec558cc8f
phillserrazina <fserrazina99@gmail.com>
2019-07-26 16:14:29 +0100

    Added Main Menu Functionality; Began Revamping Attack System

--- ClawsAndPawsProject/Assets/Scripts/Scriptable Objects/CharacterSO.cs
+++ ClawsAndPawsProject/Assets/Scripts/Scriptable Objects/CharacterSO.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 [CreateAssetMenu(menuName="Character Data", fileName="New Character Data")]
 public class CharacterSO : ScriptableObject {
 
-	public string actorName;
+	public string actorName;	// Have this be read from a save file
 	public float healthPoints;
 	public float staminaPoints;
 	public float speedPoints;
