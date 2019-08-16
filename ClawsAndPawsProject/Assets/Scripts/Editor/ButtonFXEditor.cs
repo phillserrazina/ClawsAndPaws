43c385dae6beb219f4a172efdc374c0b626cc600
phillserrazina <fserrazina99@gmail.com>
2019-08-16 13:21:11 +0100

    Upgraded the ButtonFX Script

---
+++ ClawsAndPawsProject/Assets/Scripts/Editor/ButtonFXEditor.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+
+[CanEditMultipleObjects]
+[CustomEditor(typeof(ButtonFX))]
+public class ButtonFXEditor : Editor
+{
+    private ButtonFX buttonScript;
+
+    private void OnEnable() {
+        buttonScript = target as ButtonFX;
+    }
+
+    public override void OnInspectorGUI() {
+        buttonScript.sfxName = EditorGUILayout.TextField("SFX Name: ", buttonScript.sfxName);
+
+        EditorGUILayout.BeginHorizontal();
+        buttonScript.changeSize = EditorGUILayout.Toggle("Change Size: ", buttonScript.changeSize);
+
+        if (buttonScript.changeSize) {
+            buttonScript.newSize = EditorGUILayout.FloatField(buttonScript.newSize);
+        }
+        EditorGUILayout.EndHorizontal();
+
+        EditorGUILayout.BeginHorizontal();
+
+        buttonScript.changeSprite = EditorGUILayout.Toggle("Change Sprite: ", buttonScript.changeSprite);
+
+        if (buttonScript.changeSprite) {
+            buttonScript.newSprite = (Sprite)EditorGUILayout.ObjectField(buttonScript.newSprite, typeof(Sprite), false);
+        }
+
+        EditorGUILayout.EndHorizontal();
+
+        if (GUI.changed) {
+            EditorUtility.SetDirty(buttonScript);
+            EditorSceneManager.MarkSceneDirty(buttonScript.gameObject.scene);
+        }
+    }
+}
--- ClawsAndPawsProject/Assets/Scripts/UI/ButtonFX.cs
+++ ClawsAndPawsProject/Assets/Scripts/UI/ButtonFX.cs
@@ -2,12 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public class ButtonFX : MonoBehaviour, IPointerEnterHandler
+public class ButtonFX : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
-    [SerializeField] private string sfxName;
+    public string sfxName;
+    public bool changeSize = false;
+    public float newSize;
+    public bool changeSprite = false;
+    public Sprite newSprite;
+
+    private Sprite originalSprite;
+
+    private void Awake() {
+        originalSprite = GetComponent<Image>().sprite;
+    }
 
     public void OnPointerEnter(PointerEventData eventData) {
         FindObjectOfType<AudioManager>().Play(sfxName);
+
+        if (changeSize) transform.localScale *= newSize;
+        if (changeSprite) GetComponent<Image>().sprite = newSprite;
+    }
+
+    public void OnPointerExit(PointerEventData eventData) {
+        if (changeSize) transform.localScale /= newSize;
+        if (changeSprite) GetComponent<Image>().sprite = originalSprite;
     }
 }
