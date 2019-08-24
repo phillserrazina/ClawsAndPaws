78c8ba9af7c4517f40d82c7d9cee12e04550a43f
phillserrazina <fserrazina99@gmail.com>
2019-08-24 11:41:11 +0100

    Added link to credits scene

---
+++ ClawsAndPawsProject/Assets/Scripts/UI/OpenLinkButton.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenLinkButton : MonoBehaviour
+{
+    public void OpenLink(string link) {
+        Application.OpenURL(link);
+    }
+}
